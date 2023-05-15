4ffbd5bd0774d48538be6db54837b9c7eb6a7626
alexiscyber14 <142sledgehammer14@gmail.com>
2023-05-15 13:42:04 +0100

    add data

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,28 +21,7 @@ CREATE TABLE medical_histories (
   patient_id INT,
   FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
-/*invoices table */
 
-CREATE TABLE invoices (
-  id INT PRIMARY KEY,
-  total_amount DECIMAL,
-  generated_at TIMESTAMP,
-  paid_at TIMESTAMP,
-  medical_history_id INT,
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
-);
-
-/* invoices items table */
-CREATE TABLE invoice_items (
-  id INT PRIMARY KEY,
-  unit_price DECIMAL,
-  quantity INT,
-  total_price DECIMAL,
-  invoice_id INT,
-  treatment_id INT,
-  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
-  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
-);
 
 
 
